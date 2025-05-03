5b2aa00f30097846c6c5e594e03ff440d2ef414e
Ricardo DB <ricardo.domber96@gmail.com>
2025-05-02 18:15:27 -0600

    More fixes

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ mongoose
 
 
 // Iniciar servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
