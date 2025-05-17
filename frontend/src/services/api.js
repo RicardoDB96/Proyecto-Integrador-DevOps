d60615237f489b38df15105f7420a23c77584f2a
Ricardo DB <ricardo.domber96@gmail.com>
2025-05-16 18:49:38 -0600

    Change ip

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 //frontend/src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://3.82.18.121:5000/api'; //'http://localhost:5000/api';   // ✅ Ajusta esto con la IP correcta
+const API_URL = 'http://34.207.75.197:5000/api'; //'http://localhost:5000/api';   // ✅ Ajusta esto con la IP correcta
 
 const api = axios.create({
   baseURL: API_URL,
